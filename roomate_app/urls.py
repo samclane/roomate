6f2ddb59763fff92797fde4d3d5c03285b3bb7b7
cdemmm <ba10b44a@opayq.com>
2016-12-06 15:50:18 -0600

    Update urls.py

--- roomate_app/urls.py
+++ roomate_app/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     url(r'^logout/', auth_views.logout, {'next_page': '/roomate_app'}, name='logout'),
     url(r'^create_user/', views.create_user, name='create_user'),
     url(r'^view_bills/', views.view_bills, name='view_bills'),
+    url(r'^view_grocery/', views.view_grocery, name='view_grocery'),
+    url(r'^view_chores/', views.view_chores, name='view_chores'),
 ]
