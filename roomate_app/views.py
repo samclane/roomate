085098fcfb729ede05e5f6afee59499f9ebd6b3c
cdemmm <ba10b44a@opayq.com>
2016-12-06 10:34:19 -0600

    Update views.py

--- roomate_app/views.py
+++ roomate_app/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .forms import CreateUserForm
+from .models import Bill, Grocery, Chore
 
 
 def index(request):
